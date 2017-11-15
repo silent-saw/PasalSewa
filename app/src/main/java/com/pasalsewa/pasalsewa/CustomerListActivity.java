043856e4e8236397eebf683bcc8805e6dd9afd32
Anjan <zaaaneanjan@gmail.com>
2017-11-15 22:27:21 +0545

    Addition of CustomerInfo

--- app/src/main/java/com/pasalsewa/pasalsewa/CustomerListActivity.java
+++ app/src/main/java/com/pasalsewa/pasalsewa/CustomerListActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class CustomerListActivity extends AppCompatActivity {
 
@@ -27,9 +29,29 @@ public class CustomerListActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    TextView customer1,customer2,customer3,customer4,customer5,customer6,createNewCustomer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+        customer1= (TextView) findViewById(R.id.customer1);
+        customer2= (TextView) findViewById(R.id.customer2);
+        customer3= (TextView) findViewById(R.id.customer3);
+        customer4= (TextView) findViewById(R.id.customer4);
+        customer5= (TextView) findViewById(R.id.customer5);
+        customer6= (TextView) findViewById(R.id.customer6);
+        createNewCustomer= (TextView) findViewById(R.id.createNewCustomer);
+
+        customer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CustomerListActivity.this,CustomerInfoActivity.class));
+            }
+        });
+
+
+
     }
 }
