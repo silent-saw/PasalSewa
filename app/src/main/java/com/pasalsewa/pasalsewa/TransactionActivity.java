8cb304f9d22c3b706b35f1e9192d972461cd85ad
Anjan <zaaaneanjan@gmail.com>
2017-11-20 03:17:25 +0545

    Modified Sdk version from 15 to 16 Compiled recyclerview v7 and expandable recycler view 1.3 in dependencies
    Fixed the create Customer Button at footer
    Added Expand and collapse of transaction history using recyclerView

--- app/src/main/java/com/pasalsewa/pasalsewa/TransactionActivity.java
+++ app/src/main/java/com/pasalsewa/pasalsewa/TransactionActivity.java
@@ -2,12 +2,42 @@ package com.pasalsewa.pasalsewa;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.pasalsewa.pasalsewa.helper.Bill;
+import com.pasalsewa.pasalsewa.helper.Parent;
+import com.pasalsewa.pasalsewa.helper.viewholders.adapters.RecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionActivity extends AppCompatActivity {
+
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    List<Parent> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction);
+        setContentView(R.layout.transactions_list);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        getList();
+        recyclerViewAdapter = new RecyclerViewAdapter(list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(TransactionActivity.this));
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    public void getList(){
+        list = new ArrayList<>(4);
+        for(int i=0;i<4;i++){
+            List<Bill>bills= new ArrayList<>(1);
+            bills.add(new Bill("Sachin Maharjan","Credit",i+1,10000,5000,5000));
+            list.add(new Parent("2017/7/20 5:30 A.M",bills));
+        }
+
     }
 }
